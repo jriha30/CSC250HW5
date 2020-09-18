3af5f45c2ac9a2a904643a3bae986952547eea76
Jason Riha <superjar30@gmail.com>
2020-09-18 13:56:17 -0500

    finished assignment

--- src/BibleBook.java
+++ src/BibleBook.java
@@ -1,4 +1,19 @@
 public class BibleBook
 {
+	private String bookName;
+	private int numberOfChapters;
+	private String chapterSummary;
 	
+	public BibleBook(String bookName, int numberOfChapters, String chapterSummary)
+	{
+		this.bookName = bookName;
+		this.numberOfChapters = numberOfChapters;
+		this.chapterSummary = chapterSummary;
+	}
+	
+	public void display()
+	{
+		System.out.println(this.bookName + " (" + this.numberOfChapters + " Chapters)");
+		System.out.println(this.chapterSummary);
+	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -3,6 +3,12 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
+		BibleBook book1 = new BibleBook("Revelation", 22, "Description of the end times.");
+		BibleBook book2 = new BibleBook("John", 21, "An account of Jesus' life written by John.");
+		BibleBook book3 = new BibleBook("Romans", 16, "Letters written to the Romans by Paul.");
 		
+		book1.display();
+		book2.display();
+		book3.display();
 	}
 }
